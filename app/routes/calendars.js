b19b88a75daf7b60c756f90ae7f761cea2959b0e
Patrick <pdougall1@gmail.com>
2015-01-21 01:55:24 -0500

    clean up

--- app/routes/calendars.js
+++ app/routes/calendars.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
-import Calendar from '../lib/calendar/calendar';
 
 export default Ember.Route.extend({
-	// model: function () {
-	// 	return {}
-	// }
-
-	// setupController: function(controller, model) {
-	// 	debugger
-	// 	controller.set('model', model);
-
-	// 	controller.set('calendar', Calendar.create());
-	// }
 });
