5efa6663b3ac9452006c639a2762ff3064cec2cb
Patrick <pdougall1@gmail.com>
2015-01-25 12:31:11 -0500

    add logout action

---
+++ app/routes/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  actions: {
+    logout: function() {
+      this.controllerFor('sessions').reset();
+      this.transitionTo('sessions');
+    }
+  }	
+
+});
