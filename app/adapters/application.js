9137e5b9dc596807c05ebc8ff3a0aa6096e665b2
Patrick <pdougall1@gmail.com>
2015-01-25 14:58:01 -0500

    move back end domain to env var

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
+import config from '../config/environment';
 
 export default DS.ActiveModelAdapter.extend({
-	host: 'http://localhost:3000'
+	host: FunSchedularFront.FUN_SCHEDULAR_RAILS_DOMAIN
 });
