4e55d2bc6c166bbe33db331edb4ad08ceceb8d12
Patrick <pdougall1@gmail.com>
2015-01-27 19:19:39 -0500

    add  a google places component

---
+++ app/components/google-places.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.TextField.extend({
+	setupAutocomplete: function () {
+		var _this = this;
+		var autocomplete = new google.maps.places.Autocomplete(document.getElementById(this.get('elementId')));
+		this.set('autocomplete', autocomplete);
+
+		var callback = function () {
+			var googlePlace = autocomplete.getPlace();
+			_this.set('googlePlace', googlePlace);
+		}
+
+		// does the callback really need to be wrapped in a function?
+		google.maps.event.addListener(autocomplete, 'place_changed', function () { callback() });
+	}.on('didInsertElement'),
+
+	placeChange: function () {
+		this.sendAction('action', this.get('googlePlace'));
+	}.observes('googlePlace')
+
+});
